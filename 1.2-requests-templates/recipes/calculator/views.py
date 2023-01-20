a34074a3e412b7bc3ea9645b69e7698bb65e3039
Yury Vereskun <satriks@mail.ru>
2023-01-20 03:28:09 +0300

    done recipt task

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render
 
 DATA = {
@@ -18,6 +19,12 @@ DATA = {
     },
     # можете добавить свои рецепты ;)
 }
+RU_DATA = {'omlet': 'Омлет', 'pasta': 'Паста', 'buter':'Бутерброд' }
+
+def recipe(request, rec):
+    person = request.GET.get('servings', 1)
+    context = {'recip' : {k:round(v * int(person), 2) for k,v in DATA[rec].items() }, 'name' : RU_DATA.get(rec).lower(), 'servings' : person}
+    return render(request, 'calculator/index.html', context)
 
 # Напишите ваш обработчик. Используйте DATA как источник данных
 # Результат - render(request, 'calculator/index.html', context)
--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 
 from django.urls import path
 
+from calculator.views import recipe
+
 urlpatterns = [
     # здесь зарегистрируйте вашу view-функцию
+    path('<rec>/', recipe)
 ]
