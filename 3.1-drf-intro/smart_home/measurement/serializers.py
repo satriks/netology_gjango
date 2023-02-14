222b75f25b3fac322c803ffdf9d94240534b3a75
Yury Vereskun <satriks@mail.ru>
2023-02-15 02:38:10 +0300

    done

--- 3.1-drf-intro/smart_home/measurement/serializers.py
+++ 3.1-drf-intro/smart_home/measurement/serializers.py
@@ -23,15 +23,9 @@ class SensorDetailSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'description', 'measurements']
 
     def create(self, validated_data):
-        """
-        Create and return a new `Snippet` instance, given the validated data.
-        """
         return Sensor.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        """
-        Update and return an existing `Snippet` instance, given the validated data.
-        """
         instance.name = validated_data.get('name', instance.name)
         instance.description = validated_data.get('description', instance.description)
         instance.save()
