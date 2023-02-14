168bcbfa3aa597d4bdb128eb7c3d14e5c84eab07
Yury Vereskun <satriks@mail.ru>
2023-02-15 02:43:19 +0300

    done

--- 3.1-drf-intro/smart_home/measurement/serializers.py
+++ 3.1-drf-intro/smart_home/measurement/serializers.py
@@ -22,11 +22,4 @@ class SensorDetailSerializer(serializers.ModelSerializer):
         model = Sensor
         fields = ['id', 'name', 'description', 'measurements']
 
-    def create(self, validated_data):
-        return Sensor.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.save()
-        return instance
