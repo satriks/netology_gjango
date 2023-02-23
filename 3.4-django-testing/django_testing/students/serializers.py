216ad5265fa8c6039d73f9ed85b4205dbd2e205f
Yury Vereskun <satriks@mail.ru>
2023-02-23 05:09:25 +0300

    fix typo

--- 3.4-django-testing/django_testing/students/serializers.py
+++ 3.4-django-testing/django_testing/students/serializers.py
@@ -11,7 +11,6 @@ class CourseSerializer(serializers.ModelSerializer):
         fields = ("id", "name", "students")
 
     def validate(self, data):
-        """Метод для валидации. Вызывается при создании и обновлении."""
 
         if len(data) > settings.MAX_STUDENTS_PER_COURSE:
             raise serializers.ValidationError('На курсе уже максимум студентов')
